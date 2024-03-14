cb68b21aa376eaff5a8c9cfa805674af4eac05d1
vvzvlad <git@vvzvlad.xyz>
2024-03-14 17:00:05 +0300

    refactoring and add /logs

--- withdrawal.py
+++ withdrawal.py
@@ -10,56 +10,23 @@ import yaml
 import sys
 import random
 import datetime
-
-start_time = datetime.datetime.now()
-success_file_path = os.path.join(os.path.dirname(__file__), f"success_{start_time.strftime('%Y-%m-%d_%H-%M-%S')}.csv") 
-error_file_path = os.path.join(os.path.dirname(__file__), f"error_{start_time.strftime('%Y-%m-%d_%H-%M-%S')}.csv") 
-
-wallets_file_path = os.path.join(os.path.dirname(__file__), 'wallets.txt')
-cred_file_path = os.path.join(os.path.dirname(__file__), 'settings.txt')
-
-# Проверка наличия файлов
-if not os.path.exists(wallets_file_path):
-    raise FileNotFoundError(f"Wallets file '{wallets_file_path}' is missing.")
-if not os.path.exists(cred_file_path):
-    raise FileNotFoundError(f"Settings file '{cred_file_path}' is missing.")
-
-# Открываем файл
-with open(cred_file_path, 'r') as file:
-    data = yaml.safe_load(file)
-
-# Проверка наличия переменных в файле и их пустые значения
-required_variables = ['API_KEY_BINANCE', 'API_SECRET_BINANCE', 'API_KEY_OKX', 'API_SECRET_OKX', 'API_PASSPHRASE_OKX', 'API_BYBIT_KEY', 'API_BYBIT_SECRET', 'time_sleep_low', 'time_sleep_max']
-missing_variables = [var for var in required_variables if var not in data or not data[var]]
-
-if missing_variables:
-    raise ValueError(f"Missing or empty required variables in settings file: {', '.join(missing_variables)}")
-
-# Получаем значения переменных
-API_KEY_BINANCE = data['API_KEY_BINANCE']
-API_SECRET_BINANCE = data['API_SECRET_BINANCE']
-API_KEY_OKX = data['API_KEY_OKX']
-API_SECRET_OKX = data['API_SECRET_OKX']
-API_PASSPHRASE_OKX = data['API_PASSPHRASE_OKX']
-API_BYBIT_KEY = data['API_BYBIT_KEY']
-API_BYBIT_SECRET = data['API_BYBIT_SECRET']
-time_sleep_low = data['time_sleep_low']
-time_sleep_max = data['time_sleep_max']
-test_mode = data['test_mode']
-
-cprint(f"Start. python_ver: {sys.version}, ccxt_ver: {ccxt.__version__}", "white")
-if random.randint(1, 100) <= 20:
-    cprint(f"Харе работать, иди проветрись", "red")
+import base64
 
 def stub_withdraw(address, amount_to_withdrawal, symbolWithdraw, network, exchange):
     cprint(f">>> Stub withdraw ok: {exchange} | {address} | {amount_to_withdrawal} | {symbolWithdraw} | {network}  ", "green")
+    transaction_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    write_to_csv(success_file_path, [transaction_time, exchange, network, symbol_withdraw, address, amount_to_withdrawal, "success"])
+    try:
+        test = 1/0
+    except Exception as error:
+        write_to_csv(error_file_path, [ transaction_time, exchange, network, symbol_withdraw, address, amount_to_withdrawal, type(error).__name__, str(error)])
 
 def write_to_csv(filename, data):
     with open(filename, mode='a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(data)
 
-def write_header():
+def write_csv_header():
     success_header = ["Transaction Time", "Exchange", "Network", "Symbol Withdraw", "Address", "Amount to Withdrawal", "Status"]
     error_header = ["Transaction Time", "Exchange", "Network", "Symbol Withdraw", "Address", "Amount to Withdrawal", "Error Type", "Error Message"]
 
@@ -182,7 +149,7 @@ network_mappings = {
         "опти": "OPTIMISM",
         "бейс": "BASE",
         "сол": "SOL",
-        "BSC": "BEP20",
+        "bsc": "BEP20",
         "матик": "MATIC",
         "function": binance_withdraw
     },
@@ -191,7 +158,7 @@ network_mappings = {
         "опти": "OPTIMISM",
         "бейс": "Base",
         "сол": "SOL",
-        "BSC": "BEP20",
+        "bsc": "BEP20",
         "матик": "MATIC",
         "function": okex_withdraw
     },
@@ -205,6 +172,61 @@ network_mappings = {
 }
 
 if __name__ == "__main__":
+    start_time = datetime.datetime.now()
+    logs_dir = os.path.join(os.path.dirname(__file__), 'logs')
+    if not os.path.exists(logs_dir):
+        os.makedirs(logs_dir)
+
+    success_file_path = os.path.join(logs_dir, f"success_{start_time.strftime('%Y-%m-%d_%H-%M-%S')}.csv")
+    error_file_path = os.path.join(logs_dir, f"error_{start_time.strftime('%Y-%m-%d_%H-%M-%S')}.csv")
+
+
+    wallets_file_path = os.path.join(os.path.dirname(__file__), 'wallets.txt')
+    cred_file_path = os.path.join(os.path.dirname(__file__), 'settings.txt')
+
+    # Проверка наличия файлов
+    if not os.path.exists(wallets_file_path):
+        raise FileNotFoundError(f"Wallets file '{wallets_file_path}' is missing.")
+    if not os.path.exists(cred_file_path):
+        raise FileNotFoundError(f"Settings file '{cred_file_path}' is missing.")
+
+    # Открываем файл
+    with open(cred_file_path, 'r') as file:
+        data = yaml.safe_load(file)
+
+    # Проверка наличия переменных в файле и их пустые значения
+    required_variables = ['API_KEY_BINANCE', 'API_SECRET_BINANCE', 'API_KEY_OKX', 'API_SECRET_OKX', 'API_PASSPHRASE_OKX', 'API_BYBIT_KEY', 'API_BYBIT_SECRET', 'time_sleep_low', 'time_sleep_max']
+    missing_variables = [var for var in required_variables if var not in data or not data[var]]
+
+    if missing_variables:
+        raise ValueError(f"Missing or empty required variables in settings file: {', '.join(missing_variables)}")
+
+    # Получаем значения переменных
+    API_KEY_BINANCE = data['API_KEY_BINANCE']
+    API_SECRET_BINANCE = data['API_SECRET_BINANCE']
+    API_KEY_OKX = data['API_KEY_OKX']
+    API_SECRET_OKX = data['API_SECRET_OKX']
+    API_PASSPHRASE_OKX = data['API_PASSPHRASE_OKX']
+    API_BYBIT_KEY = data['API_BYBIT_KEY']
+    API_BYBIT_SECRET = data['API_BYBIT_SECRET']
+    time_sleep_low = data['time_sleep_low']
+    time_sleep_max = data['time_sleep_max']
+    test_mode = data['test_mode']
+
+    cprint(f"Start. python_ver: {sys.version}, ccxt_ver: {ccxt.__version__}", "white")
+    if random.randint(1, 100) <= 20:
+        selected_message_encoded = random.choice([
+            '0JLRgNC10LzRjyDRgdC80L7RgtGA0LXRgtGMINC40L3RgdGC0LDQs9GA0LDQvA==',
+            '0JjQtNC4INC/0L7Qs9C70LDQtNGMINC60L7RiNC60YMs INC+0L3QsCDRg9C20LUg0YHQutGD0YfQsNC10YI=',
+            '0J3QtSDQsdGD0LTRjCDQttC+0L/QvtC5Lg==',
+            '0KHQtNC10LvQsNC5INGH0LDQuSDQvdCw0LrQvtC90LXRhiwg0YDQsNCx0L7RgtCw0YLRjCDQvdCwINGB0YPRhdGD0Y4g4oCU INC/0L7Qu9C90YvQuSDQv9C40LfQtNC10YYu',
+            '0J/QvtGA0LAg0LIg0YHQv9C+0YDRgtC30LDQuy4uLiDRjdGN0Y0s INGF0L7RgtGP INCx0Ysg0LTQviDRhdC+0LvQvtC00LjQu9GM0L3QuNC60LAg0Lgg0L7QsdGA0LDRgtC90L4u IA==',
+            '0J/QvtGB0LzQvtGC0YDQuCDQsiDQvtC60L3Qviwg0LLQtNGA0YPQsyDRgtCw0Lwg0YfRgtC+LdGC0L4g0LjQt9C80LXQvdC40LvQvtGB0Ywg0LfQsCDQv9C+0YHQu9C10LTQvdC40LUgMTAg0YfQsNGB0L7Qsi4=',
+            '0J3QtSDQs9C+0YDQsdC40YHRjCE='])
+        selected_message = base64.b64decode(selected_message_encoded).decode("utf-8")
+        print(selected_message)
+
+    write_csv_header()
 
     with open(wallets_file_path, 'r', encoding='utf-8', newline='') as tsvfile:
         reader = csv.reader(tsvfile, delimiter='\t')
